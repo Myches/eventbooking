49831fef6f52a07ef5abc5b8ec3b9e40ff90faaf
Michael Andorful <andorfulmichaelebo@gmail.com>
2025-07-28 19:57:49 +0000

    made changes to passport js

--- config/passport.js
+++ config/passport.js
@@ -1,19 +1,10 @@
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 import db from './db.js';
-import dotenv from "dotenv";
-
-
-dotenv.config();
-// Ensure JWT_SECRET is defined
-if (!process.env.JWT_SECRET) {
-    console.error('Error: JWT_SECRET is not defined in environment variables');
-    throw new Error('JWT_SECRET is required for JwtStrategy');
-}
 
 const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.JWT_SECRET,
+    secretOrKey: 'a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6' 
 };
 
 passport.use(
